d9d9884c0b756da3531353f450e62a2d4c7d7e0f
rosa <rosahadi19@gmail.com>
2024-11-27 22:41:54 +0300

    feat: implement ICO marketplace contract with token sale, purchase, and management functionality
    - Added support for creating ICO sales with dynamic token pricing
    - Integrated ERC20 token transfers and balance retrieval
    - Implemented modifiers for ownership and token validation checks
    - Included event emissions for token transactions and withdrawals
    - Added functions to get all tokens, supported tokens, and tokens by creator

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -33,56 +33,132 @@ contract ICOMarketplace {
   event TokenAdded(address indexed token, uint256 price, address indexed creator,
     string name, string symbol);
 
-
   // MODIFIERS
 
   modifier supportedToken(address _token) {
-    
+    require(tokenDetails[_token].supported == true, "Token not Supprted");
+    _;
   }
 
   modifier onlyOwner() {
-
+    require(msg.sender == owner, "caller not owner");
+    _;
   }
 
   modifier onlyCreator(address _token) {
-
+    require(msg.sender == tokenDetails[_token].creator, "caller not creator");
+    _;
   }
 
   receive() external payable {
-
+     revert("contract does not accept Ether directly");
   }
 
   constructor() {
-
+      owner =msg.sender;
   }
 
   //CONTRACT FUNCTIONS
 
-  function createICOSale(address _token, uint256 _price) {
+  function createICOSale(address _token, uint256 _price) external {
+        IERC20 token = IERC20(_token);
+        string memory tokenName = token.name();
+        string memory tokenSymbol = token.symbol();
 
+        tokenDetails[_token] = TokenDetails({
+        token: _token,
+        supported: true,
+        price: _price,
+        name: tokenName,
+        creator: msg.sender,
+        symbol: tokenSymbol
+        });
+
+        emit TokenAdded(_token, _price, msg.sender, tokenName, tokenSymbol);
   }
 
   function multiply(uint256 x, uint256 y) internal pure returns(uint256 z) {
-
+    require(y == 0 || (z = x * y) / y == x);
   } 
 
-  function buyToken(address _token, uint256 _amount) external payable supportedToken(_token) {}
+  function buyToken(address _token, uint256 _amount) external payable supportedToken(_token) {
+    require(_amount > 0, "Amount must be greater that 0");
 
-  function getBalance(address _token) external view returns(uint256){}
+    TokenDetails memory details = tokenDetails[_token];
+    uint256 totalCost = multiply(details.price, _amount);
+    require(msg.value == totalCost, "Incorrect Ether amount");
+
+    // TRANSFER THE PAYMENT TO THE TOKEN CREATOR 
+    (bool sent,) = details.creator.call{value: msg.value}("");
+    require(sent, "failed to transfer Ether to token creator");
+
+    IERC20 token = IERC20(_token);
+    require(token.transfer(msg.sender, _amount * 10**18), "Transfer failed");
+
+    emit TokenTransferred(_token, msg.sender, _amount);
+  }
+
+  function getBalance(address _token) external view returns(uint256){
+    require(tokenDetails[_token].supported, "Token not supported");
+
+    IERC20 token = IERC20(_token);
+    return token.balanceOf(address(this));
+  }
 
   function getSupportedTokens() external view returns(address[] memory) {
-
+    return allSupportedTokens;
   }
 
-  function withdraw(address _token, uint256 _amount) external onlyCreator(_token) supportedToken(_token){
+  function withdrawToken(address _token, uint256 _amount) external onlyCreator(_token) supportedToken(_token){
+    require(_amount > 0, "Amount must be greatet than 0");
+    
+      IERC20 token = IERC20(_token);
+      uint256 balance = token.balanceOf(address(this));
+      require(balance >= 0, 'Balance must be greatet than 0');
+      require(token.transfer(msg.sender, _amount), "Transfer failed");
 
+        emit TokenWithdraw(_token, msg.sender, _amount);
   }
 
-  function getTokenDetails(address) external view returns(TokenDetails memory) {
+  function getTokenDetails(address _token) external view returns(TokenDetails memory) {
+    require(tokenDetails[_token].supported , "Token not supported");
 
+    return tokenDetails[_token];
   }
 
-  function getTokenCreatedBy(address _creator) external view returns(TokenDetails[] memory) {}
+  function getTokenCreatedBy(address _creator) external view returns(TokenDetails[] memory) {
+    uint256 count = 0;
 
-  function getAllTokens() external view returns(TokenDetails[] memory) {}
- }
+    for( uint256 i= 0; i< allSupportedTokens.length; i++) {
+      if(tokenDetails[allSupportedTokens[i]].creator == _creator)
+      {
+        count++;
+      }
+    }
+
+    TokenDetails[] memory tokens = new TokenDetails[](count);
+    uint256 index = 0;
+
+    for( uint256 i= 0; i< allSupportedTokens.length; i++) {
+      if(tokenDetails[allSupportedTokens[i]].creator == _creator)
+      {
+        tokens[index] = tokenDetails[allSupportedTokens[i]];
+        index++;
+      }
+    }
+
+    return tokens;
+  }
+
+  function getAllTokens() external view returns(TokenDetails[] memory) {
+    uint256 length = 0;
+
+    TokenDetails[] memory tokens = new TokenDetails[](length);
+    
+    for( uint256 i= 0; i< 0; i++) {
+      tokens[i] = tokenDetails[allSupportedTokens[i]];
+    }
+
+    return tokens;
+  }
+}
