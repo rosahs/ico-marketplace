e26f72d32fc3c4fde751290f217e4ff325301393
rosa <rosahadi19@gmail.com>
2024-11-27 23:58:35 +0300

    feat: add custom ERC20 token contract with initial supply mint

---
+++ contracts/ERC20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    constructor()
+        ERC20("ERC20Token", "TBC")
+        _mint(msg.sender, 50000**18);
+    {}
+
+}
